6f88ede7b2d02eeaec8ab64a527266efdd02f472
iknowca <iknowca@gmail.com>
2023-05-21 03:17:17 +0900

    feat: [김원기] footer 생성 [VUEx]

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,15 @@
         <v-main>
             <router-view align="center"/>
         </v-main>
+        <v-footer padless>
+            <v-col class="align-center text-center">
+                    <v-btn icon :href='`https://github.com/eddi-study/wyj-front`'>
+                        <v-icon size="34px">mdi-github</v-icon>
+                    </v-btn>
+                <v-card-text class="pt-0 black--text">WYJ</v-card-text>
+
+            </v-col>
+        </v-footer>
     </v-app>
 </template>
 
