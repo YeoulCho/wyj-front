e360ff52696940e57ddf015d8635ca75875b2336
YeoulCho <cyu5733@gmail.com>
2023-05-22 00:40:59 +0900

    feat: [조여울] 자유게시판 만들기 [VUE-BOARD-LIST-1]

--- src/store/board/action.js
+++ src/store/board/action.js
@@ -3,18 +3,20 @@ import {REQUEST_BOARD_LIST_TO_SPRING} from "@/store/board/mutation-types";
 
 export default{
     requestWriteBoardToSpring({},payload){
-        return axiosInst.post('/board/write-board', payload)
-            .then((res) => {
-                alert("게시물 등록완료")
-            })
-            .catch(() => {
-                alert("문제발생")
-            })
-    },
-    requestBoardListToSpring ({ commit }) {
-        axiosInst.get('/board/list')
-            .then((res) => {
-                commit(REQUEST_BOARD_LIST_TO_SPRING, res.data)
-            })
-    },
+    
+    return axiosInst.post('/board/write-board',payload)
+    .then((res)=>{
+        alert("게시물 등록완료")
+    })
+    .catch(()=>{
+        alert("문제발생")
+    })
+},
+    requestBoardListToSpring({commit}){
+    axiosInst.get('/board/list-board')
+    .then((res)=>{
+        commit(REQUEST_BOARD_LIST_TO_SPRING, res.data)
+    })
+    }
+
 }
